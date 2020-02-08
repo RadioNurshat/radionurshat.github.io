e38a589a2f33eea68ece0ca2e2e745d138d5392f
RNLabs <silver2prod@gmail.com>
2020-02-08 22:46:12 +0500

    New era

---
+++ precache-manifest.75f05943a1332c12e8d2eac3e579da5d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fc82e1b07d17a0100ab5b07ef65189b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "04418d58acb98039bd69",
+    "url": "/static/css/2.2cfa1b23.chunk.css"
+  },
+  {
+    "revision": "b425678e01f65bee7190",
+    "url": "/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "04418d58acb98039bd69",
+    "url": "/static/js/2.942e6115.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.942e6115.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b425678e01f65bee7190",
+    "url": "/static/js/main.9bee9765.chunk.js"
+  },
+  {
+    "revision": "1b08ae712c2133fe44c5",
+    "url": "/static/js/runtime-main.e590c699.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/static/media/persik.4e1ec840.png"
+  }
+]);
